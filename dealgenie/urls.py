7b7daaa52c22652ef29a81f2d38fdd0a0e51f207
Jonathan Sudlow <jsudlow@gmail.com>
2015-02-04 14:30:34 -0600

    First view with namespaced urls

--- dealgenie/urls.py
+++ dealgenie/urls.py
@@ -2,12 +2,15 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'dealgenie.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', include('products.urls')),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
